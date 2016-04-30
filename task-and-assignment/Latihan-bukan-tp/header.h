cbb6afa45471ede5acd27a834811d65747644fdf
Agung Sugiarto <agungsugiarto666@gmail.com>
2016-04-30 23:13:48 +0700

    Added files via upload

--- task-and-assignment/Latihan-bukan-tp/header.h
+++ task-and-assignment/Latihan-bukan-tp/header.h
@@ -1,7 +1,15 @@
+/*Saya [Agung Sugiarto] dengan nim [1505352], bersumpah bahwa saya tidak
+melakukan kecurangan seperti yang telah dispesifikasikan dalam mengerjakan 
+Tugas Praktikum Bukan TP (Latihan Sequential file) Algoritma & Pemrograman II. Jika saya melakukan
+kecurangan maka Allah menjadi saksi saya, dan sayasiap menerima 
+konsekuensinya*/
+
+//I INCLUDING THE LIB
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+//II DECLARING STRUCTURED TYPE
 typedef struct
 {
 	char nim[15];
@@ -9,9 +17,11 @@ typedef struct
 	char kelas[5];
 } mahasiswa ;
 
+//III DECLARING THE GLOBAL VARIABLE
 mahasiswa data[666];
 int n;
 
+//III THE PROCEDURES & THE FUNCTIONS
 int longestnam();
 int longestnim(); 
 int longestcla();
